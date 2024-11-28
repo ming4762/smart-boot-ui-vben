119bb8024411575823676405df575fd551ac1638
shizhongming <zhongming4762@hotmail.com>
2024-11-28 21:03:45 +0800

    feat(@vben/web-antd): 增加部门树组件：SysDeptTree

--- apps/web-antd/src/components/index.ts
+++ apps/web-antd/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as SmartAuthButton } from './smart-auth-button/smart-auth-button.vue';
 export * from './smart-table-select';
+export * from './sys-dept';
 
 export type * from './type';
