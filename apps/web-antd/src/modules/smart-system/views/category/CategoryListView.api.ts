a837ba9459b98d7be23325baf467e8765ae7a755
shizhongming <zhongming4762@hotmail.com>
2024-12-01 21:31:00 +0800

    feat(@vben/web-antd): 分类字典功能适配

---
+++ apps/web-antd/src/modules/smart-system/views/category/CategoryListView.api.ts
@@ -0,0 +1,49 @@
+import type { Recordable } from '@vben/types';
+
+import { ApiServiceEnum, requestClient } from '#/api/request';
+
+enum Api {
+  batchDeleteById = '/sys/category/batchDeleteById',
+  getById = '/sys/category/getById',
+  list = '/sys/category/listFilterTenant',
+  saveUpdate = '/sys/category/saveUpdate',
+}
+
+export const listApi = (params: Recordable<any> = {}, parentId = 0) => {
+  if (params) {
+    params = {
+      ...params,
+      parameter: {
+        ...params.parameter,
+        'parentId@=': parentId,
+      },
+    };
+  }
+  return requestClient.post(
+    Api.list,
+    { sortName: 'seq', ...params },
+    {
+      service: ApiServiceEnum.SMART_SYSTEM,
+    },
+  );
+};
+
+export const saveUpdateApi = (data: any) => {
+  return requestClient.post(Api.saveUpdate, data, {
+    service: ApiServiceEnum.SMART_SYSTEM,
+  });
+};
+
+export const getByIdApi = async (id: number) => {
+  const result = await requestClient.post(Api.getById, id, {
+    service: ApiServiceEnum.SMART_SYSTEM,
+  });
+  result.parentName = result.parent?.categoryName || '根目录';
+  return result;
+};
+
+export const deleteApi = (idList: number[]) => {
+  return requestClient.post(Api.batchDeleteById, idList, {
+    service: ApiServiceEnum.SMART_SYSTEM,
+  });
+};
---
+++ apps/web-antd/src/modules/smart-system/views/category/CategoryListView.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+import { $t as t } from '@vben/locales';
+import { useUserStore } from '@vben/stores';
+
+import {
+  type SmartTableActionItem,
+  SmartVxeTableAction,
+  useSmartTable,
+} from '#/adapter/smart-table';
+
+import {
+  deleteApi,
+  getByIdApi,
+  listApi,
+  saveUpdateApi,
+} from './CategoryListView.api';
+import {
+  getFormSchemas,
+  getSearchFormSchemas,
+  getTableColumns,
+} from './CategoryListView.config';
+
+const { getIsPlatformTenant } = useUserStore();
+
+const [SmartTable, tableApi] = useSmartTable({
+  id: 'system-category-list-view',
+  columns: getTableColumns(),
+  border: true,
+  height: 'auto',
+  useSearchForm: true,
+  pagerConfig: true,
+  rowConfig: {
+    keyField: 'id',
+    isCurrent: true,
+  },
+  treeConfig: {
+    parentField: 'parentId',
+    lazy: true,
+    reserve: true,
+    rowField: 'id',
+    loadMethod: async ({ row }) => {
+      return listApi({}, row.id);
+    },
+  },
+  searchFormConfig: {
+    schema: getSearchFormSchemas(),
+    searchWithSymbol: true,
+    actionWrapperClass: 'text-left col-span-1 pb-2 ml-1.5',
+    wrapperClass: 'flex',
+    commonConfig: {
+      labelWidth: 90,
+      formItemClass: 'pb-2',
+      componentProps: {
+        style: {
+          maxWidth: '150px',
+        },
+      },
+    },
+  },
+  addEditConfig: {
+    formConfig: {
+      schema: getFormSchemas(getIsPlatformTenant),
+    },
+  },
+  toolbarConfig: {
+    refresh: true,
+    column: { columnOrder: true },
+    zoom: true,
+    sizeSetting: true,
+    buttons: [
+      {
+        code: 'ModalAdd',
+        props: {
+          onClick: () =>
+            tableApi.showAddModal({ parentId: 0, parentName: '根节点' }),
+        },
+      },
+    ],
+  },
+  sortConfig: {
+    remote: true,
+    defaultSort: {
+      field: 'seq',
+      order: 'asc',
+    },
+  },
+  proxyConfig: {
+    ajax: {
+      query: (params) => listApi(params.ajaxParameter),
+      save: ({ body: { insertRecords, updateRecords } }) =>
+        saveUpdateApi([...insertRecords, ...updateRecords][0]),
+      delete: ({ body: { removeRecords } }) =>
+        deleteApi(removeRecords.map((item) => item.id)),
+      getById: (params) => getByIdApi(params.id),
+    },
+  },
+});
+
+const getTableActions = (row: any): SmartTableActionItem[] => {
+  return [
+    {
+      label: t('system.views.category.button.addChild'),
+      onClick: () =>
+        tableApi.showAddModal({
+          parentId: row.id,
+          parentName: row.categoryName,
+          tenantCommonYn: row.tenantCommonYn,
+        }),
+    },
+    {
+      code: 'edit',
+      onClick: () => tableApi.editByRowModal(row),
+    },
+    {
+      code: 'delete',
+      onClick: () => tableApi.deleteByRow(row),
+      danger: true,
+    },
+  ];
+};
+</script>
+
+<template>
+  <div class="page-container h-full">
+    <SmartTable>
+      <template #table-option="{ row }">
+        <SmartVxeTableAction :actions="getTableActions(row)" />
+      </template>
+    </SmartTable>
+  </div>
+</template>
+
+<style scoped></style>
