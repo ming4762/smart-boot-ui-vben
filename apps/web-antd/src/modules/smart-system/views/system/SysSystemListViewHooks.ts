4fd80744a5da445feb9fb64dfc9f136427ddc926
shizhongming <zhongming4762@hotmail.com>
2024-12-03 22:36:57 +0800

    feat(@vben/web-antd): 适配系统工具-系统管理页面

---
+++ apps/web-antd/src/modules/smart-system/views/system/SysSystemListViewHooks.ts
@@ -0,0 +1,60 @@
+import type { Recordable } from '@vben/types';
+
+import { h, ref, unref } from 'vue';
+
+import { useVbenModal } from '@vben/common-ui';
+import { $t as t } from '@vben/locales';
+
+import { SmartTableSelectUserModal } from '#/components';
+import { successMessage } from '#/utils';
+
+import { getRelatedUserIdApi, setUserApi } from './SysSystemListView.api';
+
+export const useSetUser = () => {
+  const [Modal, modalApi] = useVbenModal({
+    connectedComponent: SmartTableSelectUserModal,
+    title: '选择人员',
+  });
+  const selectUserList = ref<number[]>([]);
+  const currentSystemRef = ref<null | Recordable<any>>(null);
+
+  const handleUserSelected = async (userIdList: number[]) => {
+    selectUserList.value = userIdList;
+    try {
+      modalApi.setState({ confirmLoading: true });
+      await setUserApi({
+        userIdList,
+        systemId: unref(currentSystemRef)?.id,
+      });
+      successMessage(t('common.message.operationSucceeded'));
+      modalApi.close();
+    } finally {
+      modalApi.setState({ confirmLoading: false });
+    }
+  };
+
+  const SelectUserModal = () => {
+    return h(Modal, {
+      selectValues: unref(selectUserList),
+      onSelected: handleUserSelected,
+      class: 'w-[700px]',
+    });
+  };
+
+  const handleShowSetUser = async (row: Recordable<any>) => {
+    currentSystemRef.value = row;
+    // 查询关联的用户信息
+    modalApi.open();
+    try {
+      modalApi.setState({ loading: true });
+      selectUserList.value = await getRelatedUserIdApi(row.id);
+    } finally {
+      modalApi.setState({ loading: false });
+    }
+  };
+
+  return {
+    handleShowSetUser,
+    SelectUserModal,
+  };
+};
