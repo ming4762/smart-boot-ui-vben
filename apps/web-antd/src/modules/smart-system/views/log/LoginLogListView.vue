632bdd7950a817358a38871444d56c2295225b3a
shizhongming <zhongming4762@hotmail.com>
2024-12-03 22:06:27 +0800

    feat(@vben/web-antd): 日志管理页面适配

---
+++ apps/web-antd/src/modules/smart-system/views/log/LoginLogListView.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+/**
+ * 登录日志
+ */
+import SystemLogComponent from './SystemLogComponent.vue';
+</script>
+
+<template>
+  <SystemLogComponent id="smart-system-tool-loginLog" ident="10" />
+</template>
+
+<style scoped></style>
