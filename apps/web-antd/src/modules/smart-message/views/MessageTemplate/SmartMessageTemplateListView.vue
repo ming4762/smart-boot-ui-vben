bd0ac24f07b585b9c5460d6aa2bad5e958b9a147
shizhongming <zhongming4762@hotmail.com>
2024-12-12 09:42:38 +0800

    feat(@vben/web-antd): 适配消息模块-消息模板

---
+++ apps/web-antd/src/modules/smart-message/views/MessageTemplate/SmartMessageTemplateListView.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { useSizeSetting } from '@vben/hooks';
+
+import {
+  type SmartTableActionItem,
+  SmartVxeTableAction,
+  useSmartTable,
+} from '#/adapter/smart-table';
+
+import {
+  batchSaveUpdateApi,
+  deleteApi,
+  getByIdApi,
+  listApi,
+} from './SmartMessageTemplateListView.api';
+import {
+  getFormSchemas,
+  getSearchFormSchemas,
+  getTableColumns,
+} from './SmartMessageTemplateListView.config';
+
+const { getTableSize } = useSizeSetting();
+
+const [SmartTable, tableApi] = useSmartTable({
+  id: 'smart-message-template',
+  columns: getTableColumns(),
+  height: 'auto',
+  border: true,
+  sortConfig: {
+    remote: true,
+  },
+  pagerConfig: true,
+  columnConfig: {
+    resizable: true,
+  },
+  stripe: true,
+  rowConfig: {
+    isHover: true,
+    isCurrent: true,
+  },
+  showOverflow: 'tooltip',
+  useSearchForm: true,
+  searchFormConfig: {
+    schema: getSearchFormSchemas(),
+    searchWithSymbol: true,
+    actionWrapperClass: 'pb-2 ml-1.5',
+    wrapperClass: 'flex flex-wrap',
+    commonConfig: {
+      componentProps: {
+        style: {
+          maxWidth: '150px',
+        },
+      },
+      labelWidth: 90,
+      formItemClass: 'pb-2',
+    },
+  },
+  addEditConfig: {
+    modalConfig: {
+      fullscreen: true,
+    },
+    formConfig: {
+      schema: getFormSchemas(),
+    },
+  },
+  proxyConfig: {
+    ajax: {
+      query: (params) => listApi(params.ajaxParameter),
+      save: ({ body: { insertRecords, updateRecords } }) =>
+        batchSaveUpdateApi([...insertRecords, ...updateRecords]),
+      delete: ({ body: { removeRecords } }) => deleteApi(removeRecords),
+      getById: (params) => getByIdApi(params.id),
+    },
+  },
+  toolbarConfig: {
+    zoom: true,
+    refresh: true,
+    sizeSetting: true,
+    column: {
+      columnOrder: true,
+    },
+    buttons: [
+      {
+        code: 'ModalAdd',
+      },
+      {
+        code: 'delete',
+      },
+    ],
+  },
+});
+
+const getActions = (row: any): SmartTableActionItem[] => {
+  return [
+    {
+      code: 'edit',
+      onClick: () => {
+        tableApi.editByRowModal(row);
+      },
+    },
+  ];
+};
+</script>
+
+<template>
+  <div class="page-container h-full">
+    <SmartTable :size="getTableSize as never">
+      <template #table-operation="{ row }">
+        <SmartVxeTableAction :actions="getActions(row)" />
+      </template>
+    </SmartTable>
+  </div>
+</template>
+
+<style scoped></style>
