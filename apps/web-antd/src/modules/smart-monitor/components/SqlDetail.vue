f6d401d99cdb81f78092692329d3345e85126d81
shizhongming <zhongming4762@hotmail.com>
2025-03-02 20:10:34 +0800

    feat(project): 系统监控模块添加慢SQL监控页面

---
+++ apps/web-antd/src/modules/smart-monitor/components/SqlDetail.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  detail?: Record<string, any>;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div>{{ props.detail }}</div>
+</template>
+
+<style scoped></style>
