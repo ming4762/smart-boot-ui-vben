3060b2f8032b3c133d34a49aef88e69d1d862a07
shizhongming <zhongming4762@hotmail.com>
2025-05-04 21:01:37 +0800

    feat: 添加富文本组件SmartTinymceEditor

--- packages/effects/plugins/src/smart-tinymce/index.ts
+++ packages/effects/plugins/src/smart-tinymce/index.ts
@@ -1,1 +1,4 @@
 export { default as SmartTinymceEditor } from './tinymce-editor.vue';
+export { default as SmartTinymcePreview } from './tinymec-preview.vue';
+
+export type * from './types';
---
+++ packages/effects/plugins/src/smart-tinymce/tinymec-preview.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  content?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  content: '',
+});
+</script>
+
+<template>
+  <div class="smart-tinymce-preview" v-html="props.content"></div>
+</template>
+
+<style scoped>
+.smart-tinymce-preview {
+  :deep(ol) {
+    margin-left: 1.5em;
+    list-style-type: decimal;
+  }
+
+  :deep(ul) {
+    margin-left: 1.5em;
+    list-style-type: disc;
+  }
+}
+</style>
