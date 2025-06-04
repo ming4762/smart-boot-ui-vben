8c208a53e5edabccf190329bab7ad1605516618e
zhongming4762 <zhongming4762@hotmail.com>
2025-06-04 20:58:47 +0800

    feat(project): vben升级

--- apps/web-antd/src/modules/smart-system/views/role/components/RoleSetFunction.vue
+++ apps/web-antd/src/modules/smart-system/views/role/components/RoleSetFunction.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, unref, watch } from "vue";
+import { computed, onMounted, ref, unref, watch } from 'vue';
 
 import { $t as t } from '@vben/locales';
 import { listToTree } from '@vben/utils';
