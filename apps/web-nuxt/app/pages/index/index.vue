500c72cb406f840353ce62b8d69662f9ae48472d
zhongming4762 <zhongming4762@hotmail.com>
2026-03-04 14:29:46 +0800

    build: 移除nuxt

--- apps/web-nuxt/app/pages/index/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>zhuye11</div>
-</template>
-
-<style scoped></style>
--- apps/web-nuxt/server/api/i18n/readFrontI18n.post.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
-  const body = await readBody(event);
-  const data = await $fetch(`${config.serverUrl}/public/i18n/readFrontI18n`, {
-    method: 'POST',
-    body,
-  });
-  return data;
-});
