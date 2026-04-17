c1813909a103aa7e15212a43e307fee19908270f
zhongming4762 <zhongming4762@hotmail.com>
2026-04-17 20:33:34 +0800

    perf: 代码格式优化

--- packages/effects/layouts/src/widgets/preferences/blocks/select-item.vue
+++ packages/effects/layouts/src/widgets/preferences/blocks/select-item.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import type { SelectOption } from '@vben/types';
 
+import { useSlots } from 'vue';
+
 import { CircleHelp } from '@vben/icons';
+
 import {
   Select,
   SelectContent,
@@ -10,7 +13,6 @@ import {
   SelectValue,
   VbenTooltip,
 } from '@vben-core/shadcn-ui';
-import { useSlots } from 'vue';
 
 defineOptions({
   name: 'PreferenceSelectItem',
